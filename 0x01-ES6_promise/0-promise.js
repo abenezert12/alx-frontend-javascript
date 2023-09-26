a2204039b61a4155cca2784ba31b5f8e70d00ae7
Abenezer Tekalign <117775495+abenezert12@users.noreply.github.com>
2023-09-27 00:18:31 +0300

    TASKS

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,12 @@
+// Keep every promise you make and only make promises you can keep //
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+      if (true) {
+        resolve();
+      } else {
+        reject();
+      }
+  });
+}
+
+export default getResponseFromAPI;
