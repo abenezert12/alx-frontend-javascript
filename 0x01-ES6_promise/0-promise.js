f1dfd398a6e143420ad56caffed4dc979d57c8c5
Abenezer Tekalign <117775495+abenezert12@users.noreply.github.com>
2023-09-27 00:21:46 +0300

    TASKS

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,11 +1,13 @@
-// Keep every promise you make and only make promises you can keep //
+// Only make Promise
 function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
+    /* eslint-disable */
       if (true) {
         resolve();
       } else {
         reject();
       }
+      /* eslint-enable */
   });
 }
 
