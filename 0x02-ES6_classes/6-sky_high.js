57824108f85140f074feadbab9350b20a5f557c3
Abenezer Tekalign <117775495+abenezert12@users.noreply.github.com>
2023-09-29 22:58:56 +0300

    TASKS

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,18 +1,21 @@
 import Building from './5-building';
 
-class SkyHighBuilding extends Building {
+export default class SkyHighBuilding extends Building {
+  /* eslint-disable */
   constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
   }
 
   get floors() {
-    return (this._floors);
+    return this._floors;
+  }
+
+  set floors(Floors) {
+    this._floors = Floors;
   }
 
   evacuationWarningMessage() {
-    return (`Evacuate slowly the ${this.floors} floors`);
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
-
-export default SkyHighBuilding;
